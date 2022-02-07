47926857f659c57a842b9dd940085b5388ba868e
Saurabh Malhotra <saurabh.malhotra@gmail.com>
2022-02-07 08:21:49 -0500

    testing Toasts

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,26 +6,43 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             rollDice()
-            //toast is used for a pop up message
-            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            //toast.show()
-
         }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
+        //toast is used for a quick pop up message, falls behind if too many at a time
+        when (diceRoll) {
+            1 -> Toast.makeText(this, "You Won", Toast.LENGTH_SHORT).show()
+            2 -> Toast.makeText(this, "Sorry you lost", Toast.LENGTH_SHORT).show()
+            3 -> Toast.makeText(this, "Try again", Toast.LENGTH_SHORT).show()
+            4 -> Toast.makeText(this, "Better luck next time", Toast.LENGTH_SHORT).show()
+            5 -> Toast.makeText(this, "You win some you lose some", Toast.LENGTH_SHORT).show()
+            6 -> Toast.makeText(this, "Nope, Come back again", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
